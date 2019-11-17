5811ffc3e1d1fcc9731c26af122132db7f5dddde
b.w@gmx.tm <b.w@gmx.tm>
2019-11-17 13:01:27 +0100

    Iterator implementation

---
+++ iter.go
@@ -0,0 +1,35 @@
+package query
+
+import (
+	"reflect"
+)
+
+type ConstIter interface {
+	Peek() interface{}
+	IsEmpty() bool
+}
+
+type Iter interface {
+	ConstIter
+	Next()
+}
+
+type SliceIter struct {
+	slice interface{}
+}
+
+func (s SliceIter) Peek() interface{} {
+	val := reflect.ValueOf(s.slice)
+	return val.Index(0).Interface()
+}
+
+func (s SliceIter) IsEmpty() bool {
+	val := reflect.ValueOf(s.slice)
+	return val.Len() == 0
+}
+
+func (s *SliceIter) Next() {
+	val := reflect.ValueOf(s.slice)
+	val = val.Slice(1, val.Len())
+	s.slice = val.Interface()
+}
---
+++ iter_test.go
@@ -0,0 +1,39 @@
+package query
+
+import (
+	"testing"
+)
+
+func TestSliceIter(t *testing.T) {
+	testdata := []int{1, 2, 3, 4, 5, 6, 7}
+
+	iter := SliceIter{testdata}
+	slice := testdata[:]
+	for len(slice) != 0 {
+		assertIterIsEmpty(t, false, iter)
+		assertIterPeek(t, slice[0], iter)
+		iter.Next()
+		slice = slice[1:]
+	}
+
+	assertIterIsEmpty(t, true, &iter)
+}
+
+func assertIterIsEmpty(t *testing.T, expected bool, iter ConstIter) {
+	t.Helper()
+	if iter.IsEmpty() != expected {
+		if expected {
+			t.Errorf("Iter %#v is not empty", iter)
+		} else {
+			t.Errorf("Iter %#v is empty", iter)
+		}
+	}
+}
+
+func assertIterPeek(t *testing.T, expected int, iter ConstIter) {
+	t.Helper()
+	peeked := iter.Peek().(int)
+	if peeked != expected {
+		t.Errorf("Iter %#v: Peek returned %v, expected %v", iter, peeked, expected)
+	}
+}
